f88edbded5ab9cfa2bacc5b3dd6f4103064bd993
Goldver <goldver@gmail.com>
2016-06-11 13:51:53 +0300

    'v0.1.3'

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'Goldver'
 license          'All rights reserved'
 description      'Installs/Configures notepad-pp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 
